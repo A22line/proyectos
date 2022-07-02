3261b6a3f60cdbddd8a8fc222031c522130db48c
A22line <alinescarso@gmail.com>
2022-07-02 11:01:47 +0200

    Subiendo archivos

--- ProyectoGIT/src/com/ironhack/Main.java
+++ ProyectoGIT/src/com/ironhack/Main.java
@@ -2,6 +2,9 @@ package com.ironhack;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+
+        System.out.print ("Aline");
+        System.out.print ("Curso Java");
+
     }
 }
