1ca3f6c6dc8ea513dc3185246c02cb1ccda867b9
A22line <alinescarso@gmail.com>
2022-07-02 11:10:33 +0200

    Subiendo archivos

---
+++ proyecto2/src/com/ironrack/Main.java
@@ -0,0 +1,10 @@
+package com.ironrack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print ("Aline");
+        System.out.print ("Curso Java");
+
+    }
+}
